82d725e8540c555b8faf375b5030d38db7ccbc72
Lucassmatheu <Lucas_matheus_06@hotmail.com>
2025-03-25 13:33:50 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroPrograma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// Função principal do programa
+// name space spaço de nomes 
+
+int main()
+// função principal. Todo programa em C++ deve ter uma função main que é o ponto de entrada do programa.
+{// chave de abertura da função main
+	//{ } chaves de abertura e fechamento do bloco de código
+	// cout comando de saída de dado
+	// neste caso esta solicitando para imprimir na tela a mensagem "Primeiro Programa C++"
+	std::cout << "Primeiro Programa C++" << std::endl;
+	system("pause");
+	return 0;
+}// chave de fechamento da função main
